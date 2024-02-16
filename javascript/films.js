cfb41b6f9f9883349c7948fcf252af00be71776d
FiggeAhlberg <ahlbergfigge@gmail.com>
2024-02-16 17:24:03 +0100

    update

--- javascript/films.js
+++ javascript/films.js
@@ -1,16 +1,19 @@
 class Movie {
-  constructor(id, poster_path) {
+  constructor(id, poster_path, title, overview) {
     this.id = id;
     this.poster_path = poster_path;
+    this.title = title;
+    this.overview = overview;
   }
 }
-
 
 const movies = [];
 const imgUrl = "https://api.themoviedb.org/3/movie/";
 
 async function getMovies() {
-  const movieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
+  const api_key = "936fc4084d3ed69d2ed54442b8abc7b7";
+  const movieUrl = `
+  https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`;
 
   const response = await fetch(movieUrl);
 
@@ -19,10 +22,16 @@ async function getMovies() {
   console.log(results);
 
   for (const movie of results["results"]) {
-    const m = new Movie(movie["Id"], movie["poster_path"]);
+    const m = new Movie(
+      movie["Id"],
+      movie["poster_path"],
+      movie["title"],
+      movie["overview"]
+    );
     movies.push(m);
   }
 
+  preloadImages;
   displayMovies();
 }
 
@@ -30,13 +39,21 @@ getMovies();
 
 const allMovies = document.querySelector("#all-movies");
 
+function preloadImages() {
+  movies.forEach((movie) => {
+    const img = new Image();
+    img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  });
+}
+
 function displayMovies() {
   for (const movie of movies) {
-    const { id, poster_path } = movie;
+    const { id, poster_path, title, overview } = movie;
     const movieDiv = document.createElement("div");
     const movieImg = document.createElement("img");
 
     movieImg.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    movieImg.alt = "high-rated-movie-img";
 
     movieDiv.appendChild(movieImg);
     allMovies.appendChild(movieDiv);
@@ -59,5 +76,126 @@ function displayMovies() {
     movieDiv.addEventListener("mouseout", () => {
       movieDiv.classList.remove("border-danger");
     });
+
+    movieDiv.addEventListener("click", () => {
+      console.log("Image clicked!");
+
+      const modal = createModal(poster_path, title, overview);
+
+      document.body.appendChild(modal);
+
+      modal.style.display = "block";
+    });
+  }
+}
+
+function createModal(poster_path, title, overview) {
+  const modal = document.createElement("div");
+  modal.className = "modal fade";
+  modal.innerHTML = `
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content bg-dark">
+        <div class="modal-header border-0">
+        <h3 class="modal-title text-white">${title}</h3>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body d-flex">
+                
+          <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="featured-movie-image" class="img-fluid rounded me-3">
+          <div>
+          <h4 class="text-white">Plot</h4>
+          <p class="text-white-50">${overview}</p>
+          <button class="btn btn-primary add-btn">Add to Watchlist</button>
+          </div>
+          
+        </div>
+          
+        </div>
+      </div>
+    </div>
+  `;
+
+  modal.querySelector(".btn-close").addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) {
+      modal.style.display = "none";
+    }
+  });
+
+  modal.querySelector(".add-btn").addEventListener("click", () => {
+    addMovieToList(poster_path); // Add poster_path to the list
+    displayAddedMovies(); // Display added movies
+  });
+
+  document.body.appendChild(modal);
+
+  var myModal = new bootstrap.Modal(modal, {
+    backdrop: true,
+    focus: true,
+  });
+  myModal.show();
+
+  return modal;
+}
+
+const movieList = [];
+
+function addMovieToList(poster_path) {
+  movieList.push(poster_path);
+}
+
+function displayAddedMovies() {
+  const movieListContainer = document.querySelector("#movie-list");
+  movieListContainer.innerHTML = "";
+
+  movieList.forEach((poster_path) => {
+    const movieDiv = document.createElement("div");
+    const movieImg = document.createElement("img");
+    const buttonContainer = document.createElement("div");
+    const removeBtn = document.createElement("button");
+
+    movieImg.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+
+    removeBtn.innerText = "Remove";
+    removeBtn.classList.add(
+      "btn",
+      "btn-danger",
+      "btn-sm",
+      "remove-btn",
+      "position-absolute",
+      "mt-2"
+    );
+    removeBtn.addEventListener("click", () => {
+      removeMovieFromList(poster_path);
+      displayAddedMovies();
+    });
+
+    buttonContainer.appendChild(removeBtn);
+    movieDiv.appendChild(movieImg);
+    movieDiv.appendChild(buttonContainer);
+    movieListContainer.appendChild(movieDiv);
+
+    movieDiv.classList.add(
+      "col-1",
+      "border",
+      "rounded",
+      "p-0",
+      "me-2",
+      "ms-2",
+      "m-3",
+      "m-5"
+    );
+    movieImg.classList.add("img-fluid", "rounded");
+    buttonContainer.classList.add("d-flex", "justify-content-center");
+  });
+}
+
+function removeMovieFromList(poster_path) {
+  const index = movieList.indexOf(poster_path);
+  if (index !== -1) {
+    movieList.splice(index, 1);
   }
 }
--- javascript/home.js
+++ javascript/home.js
@@ -1,42 +1,135 @@
 function displayImages() {
   const featuredMovies = document.querySelector("#featured-container");
-  const imagesUrls = [
-    
-    "../images/leon-the-professional.webp",
-    "../images/requiem.webp",
-    "../images/perfect_blue.webp",
-    "../images/eternal_sunshine.webp",
-    "../images/pulp_fiction.webp",
-    "../images/ems.webp",
-    "../images/parasite_movie_cover.webp",
-    "../images/good_bad_ugly.webp"
+  const movieData = [
+    {
+      imageUrl: "../images/leon-the-professional.webp",
+      title: "Leon the Professional",
+      description:
+        "12-year-old Mathilda is reluctantly taken in by Léon, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade.",
+    },
+    {
+      imageUrl: "../images/requiem.webp",
+      title: "Requiem for a Dream",
+      description:
+        "The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.",
+    },
+    {
+      imageUrl: "../images/perfect_blue.webp",
+      title: "Perfect Blue",
+      description:
+        "A pop singer gives up her career to become an actress, but she slowly goes insane when she starts being stalked by an obsessed fan and what seems to be a ghost of her past.",
+    },
+    {
+      imageUrl: "../images/eternal_sunshine.webp",
+      title: "Eternal Sunshine of the Spotless Mind",
+      description:
+        "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories for ever.",
+    },
+    {
+      imageUrl: "../images/pulp_fiction.webp",
+      title: "Pulp Fiction",
+      description:
+        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    },
+    {
+      imageUrl: "../images/ems.webp",
+      title: "Oldboy",
+      description:
+        "After being kidnapped and imprisoned for fifteen years, Oh Dae-Su is released, only to find that he must find his captor in five days.",
+    },
+    {
+      imageUrl: "../images/parasite_movie_cover.webp",
+      title: "Parasite",
+      description:
+        "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    },
+    {
+      imageUrl: "../images/good_bad_ugly.webp",
+      title: "The Good, the Bad and the Ugly",
+      description:
+        "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
+    },
   ];
-  for (const image of imagesUrls) {
+  for (const movie of movieData) {
     const imgDiv = document.createElement("div");
     const img = document.createElement("img");
-    const infoBox = document.createElement("div");
 
-    img.src = image;
+    img.src = movie.imageUrl;
     img.alt = "featured-movie-image";
 
+    img.width = 300;
+    img.height = 450;
+
     imgDiv.appendChild(img);
-    imgDiv.appendChild(infoBox);
     featuredMovies.appendChild(imgDiv);
 
     imgDiv.classList.add("col", "border", "rounded", "p-0", "me-2", "ms-2");
     img.classList.add("img-fluid", "rounded");
-    infoBox.classList.add("info-box");
 
     imgDiv.addEventListener("mouseover", () => {
       imgDiv.classList.add("border-danger");
-      infoBox.style.display = "block";
     });
 
     imgDiv.addEventListener("mouseout", () => {
       imgDiv.classList.remove("border-danger");
-      infoBox.style.display = "none";
+    });
+
+    imgDiv.addEventListener("click", () => {
+      console.log("Image clicked!");
+
+      const modal = createModal(movie.imageUrl, movie.title, movie.description);
+
+      document.body.appendChild(modal);
+
+      modal.style.display = "block";
     });
   }
 }
 
 displayImages();
+
+function createModal(imageUrl, title, description) {
+  const modal = document.createElement("div");
+  modal.className = "modal fade";
+  modal.innerHTML = `
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content bg-dark">
+        <div class="modal-header border-0">
+        <h3 class="modal-title text-white">${title}</h3>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body d-flex">
+                
+          <img src="${imageUrl}" alt="featured-movie-image" class="img-fluid rounded me-3">
+          <div>
+          <h4 class="text-white">Plot</h4>
+          <p class="text-white-50">${description}</p>
+          </div>
+          
+        </div>
+          
+        </div>
+      </div>
+    </div>
+  `;
+
+  modal.querySelector(".btn-close").addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) {
+      modal.style.display = "none";
+    }
+  });
+
+  document.body.appendChild(modal);
+
+  var myModal = new bootstrap.Modal(modal, {
+    backdrop: true,
+    focus: true,
+  });
+  myModal.show();
+
+  return modal;
+}
